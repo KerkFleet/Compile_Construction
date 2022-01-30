62740bdbddd6c8e54b17a3a6799ff59851bfffd8
KerkFleet <Kerkvliet21@gmail.com>
2022-01-30 13:23:35 -0600

    Add python version checking

--- main.py
+++ main.py
@@ -10,12 +10,17 @@ from scanner import scanner
 from symbols import symbol
 import sys
 
+#check python version
+if sys.version_info[0] < 3:
+    print("Python3, or a more recent version, is required to run this script")
+    exit()
+
 # Get filename from command line
 filename=""
 if len(sys.argv) > 1:
     filename = str(sys.argv[1])
 else:
-    print("Usage: python main.py <filepath>")
+    print("Usage: python3 main.py <filepath>")
     exit()
 
 # Instantiate scanner
